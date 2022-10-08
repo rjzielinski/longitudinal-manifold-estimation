3f046b1e8d40f5c49e04563cdf7308a76d0032f7
Rob Zielinski <robert_zielinski1@brown.edu>
2022-10-07 21:21:58 -0400

    optimize norm_euclidean

--- code/subfunctions/norm_euclidean.R
+++ code/subfunctions/norm_euclidean.R
@@ -1,5 +1,5 @@
 norm_euclidean <- function(x) {
   # Norm function in a Euclidean space of any dimension
-  return(norm(matrix(x, ncol = 1), type = "F"))
-  # return(sqrt(sum(as.vector(x) ^ 2)))
+  # return(norm(matrix(x, ncol = 1), type = "F"))
+  return(sqrt(sum(as.vector(x) ^ 2)))
 }
